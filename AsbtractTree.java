77e5bb63ff21fccdebac134525d9456bbb798e39
dothuthanh <55688858+dothuthanh@users.noreply.github.com>
2020-05-14 15:46:35 +0700

    Add files via upload

---
+++ AsbtractTree.java
@@ -0,0 +1,7 @@
+package app;
+
+public abstract class AsbtractTree<E> implements Tree<E> {
+    public void inorder(){
+
+    }
+}
---
+++ Tree.java
@@ -0,0 +1,7 @@
+package app;
+
+public interface Tree<E> {
+    public boolean insert(E e);
+    public void inorder(TreeNode<E> root);
+    public  int getSize();
+}
